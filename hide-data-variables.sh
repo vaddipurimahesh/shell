22b0d93186c312f1014b8f637d50f2e11385d925
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-07 23:03:17 +0530

    Used Special Variables

--- hide-data-variables.sh
+++ hide-data-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bah
 
-echo "Please enter your usernam::"
+echo "Please enter your username::"
 read USER_NAME
 
-echo "$USER_NAME"
+echo "User name is $USER_NAME"
+
+echo "Please enter your Password::"
+read -s PASSWORD
+
