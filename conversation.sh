40bfc5df5362c3eef0397cff9aa7a1e1d6094894
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-07 22:22:24 +0530

    Conversations

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mahesh:: Hello Kishore, How are you?"
+echo "Kishore:: Hi Mahesh, I'm fine what about you?"
+echo "Mahesh:: I'm Good, What are you learning?"
+echo "Kishore:: I'm Learning Cyber security."
