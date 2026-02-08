6cc36827507eaabed2dd9a76605a220e4ad27096
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-08 09:18:11 +0530

    Used Special Variables

--- conditions.sh
+++ conditions.sh
@@ -4,4 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 25 ]; then
    echo "Given number : $NUMBER is greater than 25"
+elif [ $NUMBER -eq 25 ]; then
+   echo "Given number : $NUMBER is equal to 25"
+else 
+   echo "Given number : $NUMBER is less than 25"
 fi
