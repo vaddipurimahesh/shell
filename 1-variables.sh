2b8a6fed923063c906bcdcef31ab8ddc1e3ab5ac
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 19:14:46 +0530

    chnaged something

--- 1-variables.sh
+++ 1-variables.sh
@@ -11,4 +11,4 @@ echo "User name is :: $USER_NAME"
 echo "Please enter your Password::"
  read PASSWORD
 
- echo -e "User $R password $N is :: $PASSWORD"
+ echo -e "User$R password$N is :: $PASSWORD"
