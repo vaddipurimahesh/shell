f7811b872d3e7f522dfa89789acc012f6619d086
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 23:56:35 +0530

    chnaged something

--- conditions.sh
+++ conditions.sh
@@ -14,10 +14,10 @@ NUMBER=$1
 #  echo "The number: $NUMBER is less than 20"
 # fi
 
-if [ $NUMBER -gt $2 ]; then
- echo "The number: $NUMBER is greater than $2"
-elif [ $NUMBER -eq $2 ]; then
- echo "$NUMBER is equal to $2"
+if [ $1 -gt $2 ]; then
+ echo "The number: $1 is greater than $2"
+elif [ $1 -eq $2 ]; then
+ echo "$1 is equal to $2"
 else 
- echo "The number: $NUMBER is less than $2"
+ echo "The number: $1 is less than $2"
 fi
