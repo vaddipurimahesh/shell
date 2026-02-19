44d1b8797ecdb669d065789f0c7fd6cd6d0cfdd7
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 19:12:01 +0530

    chnaged something

--- 1-variables.sh
+++ 1-variables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+$R="\e[31m"
+$N="\e[0m"
 echo "Please enter your Username::" 
 
  read USER_NAME
@@ -9,4 +10,4 @@ echo "User name is :: $USER_NAME"
 echo "Please enter your Password::"
  read PASSWORD
 
- echo "User password is :: $PASSWORD"
+ echo "User $R password $N is :: $PASSWORD"
