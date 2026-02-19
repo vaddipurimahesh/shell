a2b9ccaf24ed87408f6df0299381e894a0d69d36
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 23:52:14 +0530

    chnaged something

--- conditions.sh
+++ conditions.sh
@@ -9,7 +9,7 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
  echo "The number: $NUMBER is greater than 20"
 elif [ $NUMBER -eq 20 ]; then
- echo "The number: $NUMBER is equal to 20"
+ echo "$NUMBER is equal to 20"
 else 
  echo "The number: $NUMBER is less than 20"
 fi
