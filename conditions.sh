f28c667afe70f2cbd8e8cf42bfd23c0c07c28c5b
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 23:54:21 +0530

    chnaged something

--- conditions.sh
+++ conditions.sh
@@ -6,10 +6,18 @@
 # -ne -> not equal
 
 NUMBER=$1
-if [ $NUMBER -gt 20 ]; then
- echo "The number: $NUMBER is greater than 20"
-elif [ $NUMBER -eq 20 ]; then
- echo "$NUMBER is equal to 20"
+# if [ $NUMBER -gt 20 ]; then
+#  echo "The number: $NUMBER is greater than 20"
+# elif [ $NUMBER -eq 20 ]; then
+#  echo "$NUMBER is equal to 20"
+# else 
+#  echo "The number: $NUMBER is less than 20"
+# fi
+
+if [ $NUMBER -gt $2 ]; then
+ echo "The number: $NUMBER is greater than $2"
+elif [ $NUMBER -eq $2 ]; then
+ echo "$NUMBER is equal to $2"
 else 
- echo "The number: $NUMBER is less than 20"
+ echo "The number: $NUMBER is less than $2"
 fi
