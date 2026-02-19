6856c74475ae2a2ea49e32a96231f53bb792603d
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-20 00:20:38 +0530

    chnaged something

--- installations.sh
+++ installations.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ]; then
  echo "Please run this script as a root user"
  exit 1
 
+fi
 
+echo "Installing nginx..."
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+ echo "Installing nginx...Failed"
+ exit 1
+else
+ echo "Installing nginx...success"
 fi
