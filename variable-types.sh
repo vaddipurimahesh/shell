594920cfa6fdc48a583016747ba4fd0accb8fc93
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 22:00:17 +0530

    chnaged something

---
+++ variable-types.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+#Special variables
+
+echo "All arguments passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of recently executed script: $!"
+echo "Exit status fof the most recent executed command: $?"
