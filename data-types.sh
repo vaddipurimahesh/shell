c2d1ec25009cf2811e4f223e05f493f6404d5640
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 22:20:48 +0530

    chnaged something

--- data-types.sh
+++ data-types.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+R="\e[31m"
+B="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+NUM1=10
+NUM2=Siva
+
+SUM=$((NUM1+NUM2))
+
+echo -e "Sum is:$Y $SUM $N"
+
 FIRST_NAME=Vaddipuri
 LAST_NAME=Mahesh
 
 FULL_NAME="$FIRST_NAME $LAST_NAME"
 
-echo "Full name is: $FULL_NAME"
+echo -e "Full name is: $B$FULL_NAME $N"
+
