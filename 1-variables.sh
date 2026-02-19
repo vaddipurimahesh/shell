7062276af611d042f9188420629d64835c31f745
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-19 19:09:03 +0530

    chnaged something

--- 1-variables.sh
+++ 1-variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Please enter your Username::" read USER_NAME
+echo "Please enter your Username::" 
+
+ read USER_NAME
 
 echo "User name is :: $USER_NAME"
+
+echo "Please enter your Password::"
+ read PASSWORD
+
+ echo "User password is :: $PASSWORD"
