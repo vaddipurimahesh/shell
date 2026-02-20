0998f9a99e97986b86109aa88884d1a8418258ba
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-20 22:14:05 +0530

    logs

--- loops.sh
+++ loops.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-for i in {1..10}
+# for i in {1..10}
+# do
+# echo $i
+# done
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripting"
+LOGS_FILE="/var/log/shell-scripting/$0.log"
+
+if [ $USERID -ne 0 ]; then
+ echo "Please run this script with root user access" | tee -a $LOGS_FILE
+ exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo " $2... Failed" | tee -a $LOGS_FILE
+    else
+     echo "$2... Success" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
 do
-echo $i
+ dnf install $package -y &>> $LOGS_FILE
+ VALIDATE $? "Installing $package" 
 done
