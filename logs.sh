55859e5a94e68684b7f7f1033a050df244e2feab
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-20 21:54:08 +0530

    logs

---
+++ logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripting"
+LOGS_FILE="/var/log/shell-scripting/$0.log"
+
+if [ $USERID -ne 0 ]; then
+ echo"Please run this script with root user access"
+ exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo " $2... Failed" &>> $LOGS_FILE
+    else
+     echo "$2... Success" &>> $LOGS_FILE
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing nginx"
