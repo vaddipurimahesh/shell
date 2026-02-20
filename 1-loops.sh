90a2f8f5f6a9f2ac8cc0c26e052f8efc36c33ade
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-20 22:30:53 +0530

    logs

---
+++ 1-loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripting"
+LOGS_FILE="/var/log/shell-scripting/$0.log"
+
+if [ $USERID -ne 0 ]; then
+ echo "Please run this script with root user access" | tee -a $LOGS_FILE
+ exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo " $2... Failed" | tee -a $LOGS_FILE
+    else
+     echo "$2... Success" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+ dnf list installed $package &>> $LOGS_FILE
+ if [ $? -ne 0 ]; then
+ echo "$package is not installed... installing now"
+  dnf install $package -y &>> $LOGS_FILE
+  VALIDATE $? "Installing $package" 
+
+ else
+  echo -e "$package already installed... $Y skipping$N"
+ fi
+done
