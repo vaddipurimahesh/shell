b904985b8c3b372e51eb0a8945ab4c0d40af7728
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-21 19:06:27 +0530

    all changes

--- backup.sh
+++ backup.sh
@@ -25,6 +25,10 @@ USAGE(){
 
 }
 
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
 if [ $# -lt 2 ]; then
  USAGE
 fi 
@@ -38,3 +42,10 @@ if [ ! -d $DEST_DIR ]; then
  echo -e "$R $DEST_DIR does not exit $N"
  exit 1
 fi 
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
