e50503e96f7b16657c841e46c60080fa9e6a7482
Mahesh Vaddipuri <maheshvaddipuri@gmail.com>
2026-02-21 18:55:12 +0530

    all changes

--- backup.sh
+++ backup.sh
@@ -3,6 +3,9 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/backup.log"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
 
 R="\e[31m"
 G="\e[32m"
@@ -17,7 +20,7 @@ fi
 mkdir -p $LOGS_FOLDER
 
 USAGE(){
-    echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <SOUR_DIR> [default14 days $N"
+    echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> [default14 days $N"
     exit 1
 
 }
@@ -26,4 +29,12 @@ if [ $# -lt 2 ]; then
  USAGE
 fi 
 
+if [ ! -d $SOURCE_DIR ]; then
+ echo -e "$R $SOURCE_DIR does not exit $N"
+ exit 1
+fi 
 
+if [ ! -d $DEST_DIR ]; then
+ echo -e "$R $DEST_DIR does not exit $N"
+ exit 1
+fi 
